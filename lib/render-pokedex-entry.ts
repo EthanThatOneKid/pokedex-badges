1289c61d5743114f9e17c3d72e4e0128d6e84248
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2021-05-03 17:14:26 -0700

    Update render-pokedex-entry.ts

--- lib/render-pokedex-entry.ts
+++ lib/render-pokedex-entry.ts
@@ -3,7 +3,7 @@ export const renderPokedexEntry = (
   species: string,
   entry: string,
 ): string =>
-  `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 50" width="200" height="50" fill="#fff" role="img" style="border-radius: 10px; border: 5px solid black;">
+  `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 50" width="195" height="45" fill="#fff" role="img" style="border-radius: 10px; border: 5px solid black;">
 <text x="2" y="13" style="font-size: 1rem;">${name}</text>
 <text x="2" y="25" style="font-size: 0.5rem;">The ${species}</text>
 <foreignObject x="90" y="-5" width="100" height="200">
