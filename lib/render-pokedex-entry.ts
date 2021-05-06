5685b9e557a3250abe42f0dd17ad6281a98bbbad
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-05-06 14:15:29 -0700

    Added bg color to svg

--- lib/render-pokedex-entry.ts
+++ lib/render-pokedex-entry.ts
@@ -3,7 +3,7 @@ export const renderPokedexEntry = (
   species: string,
   entry: string,
 ): string => {
-  return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 50" width="195" height="45" fill="#fff" role="img" style="border-radius: 10px; border: 5px solid black;">
+  return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 50" width="195" height="45" role="img" style="border-radius: 10px; border: 5px solid black; background-color: #fff;">
 <text fill="#000" x="2" y="13" style="font-size: 1rem;">${name}</text>
 <text fill="#000" x="2" y="25" style="font-size: 0.5rem;">The ${species}</text>
 <foreignObject x="90" y="-5" width="100" height="200">
