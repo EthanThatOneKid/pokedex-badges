4a5915d0f41889aae67f3dad4d71431d8a19b341
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2021-05-12 15:37:45 -0700

    Update render-index.ts

--- lib/render-index.ts
+++ lib/render-index.ts
@@ -1,5 +1,5 @@
 export const renderIndex = (): string => {
-  return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 50" width="195" height="45" role="img" style="border-radius: 10px; border: 5px solid black;  background-color: #fff;">
+  return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 50" width="180" height="30" role="img" style="border-radius: 10px; border: 5px solid black;  background-color: #fff;">
 <foreignObject x="0" y="-5" width="200" height="50">
 <p xmlns="http://www.w3.org/1999/xhtml" style="font-size: 0.5em;">
   Welcome to <code>pokedex.deno.dev</code>!
